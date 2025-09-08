0419e2c06d33796ece87851c37e8118cb534998b
Adam Cavanaugh <adamjcavanaugh@gmail.com>
2025-09-08 18:36:38 -0400

    Update HTML viewport meta tag and refine CSS styles for improved layout and responsiveness

--- questions.js
+++ questions.js
@@ -1,12 +1,12 @@
 const questions = [
-  "Average number of minutes you read each day",
-  "Average number of minutes you watch TV each day",
-  "Average number of minutes you spend commuting each day",
-  "Average number of minutes you spend exercising each day",
-  "Average number of minutes you spend on social media daily",
-  "Average number of minutes you spend talking to people in person daily",
-  "Average number of minutes you spend cooking each day",
-  "Average number of minutes you spend cleaning each day",
-  "Average number of minutes you spend gaming each day",
-  "Average number of minutes you spend listening to music each day"
+    "Average number of minutes you read each day",
+    "Average number of minutes you watch TV each day",
+    "Average number of minutes you spend commuting each day",
+    "Average number of minutes you spend exercising each day",
+    "Average number of minutes you spend on social media daily",
+    "Average number of minutes you spend talking to people in person daily",
+    "Average number of minutes you spend cooking each day",
+    "Average number of minutes you spend cleaning each day",
+    "Average number of minutes you spend gaming each day",
+    "Average number of minutes you spend listening to music each day"
 ];
